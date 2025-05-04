a6c40d994e383dc930231f9b2624765066498f38
danil <danildeis100@mail.ru>
2025-05-04 14:44:58 +0700

    23

--- project/deploy.sh
+++ project/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-IP_ADDRES='192.168.1.103'
+DEPLOY_DIR="/home/danil/project"
 
-USER='danil'
+scp -i "$HOME/.ssh/key" -o StrictHostKeyChecking=no -r ./* danil@192.168.1.103:$DEPLOY_DIR
 
-scp -i "$HOME/.ssh/key" -o StrictHostKeyChecking=no -r ./* "$USER@$IP_ADDRES:/home/danil/"
+# Подключаемся по SSH и запускаем сборку и запуск контейнеров
+ssh -i "$HOME/.ssh/key" -o StrictHostKeyChecking=no danil@192.168.1.103 << EOF
+  cd $DEPLOY_DIR
+  docker compose build
+  docker compose up -d
+EO
