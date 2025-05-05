2da87a4fdd294251ae920b92a255d224364384b3
danil <danildeis100@mail.ru>
2025-05-05 23:14:08 +0700

    server

--- project/deploy.sh
+++ project/deploy.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-USER="danil"
-IP_ADDRESS="192.168.1.103"
+USER="root"
+IP_ADDRESS="217.25.90.119"
 DEPLOY_DIR="/home/danil/project"
 
 scp -i "$HOME/.ssh/key" -o StrictHostKeyChecking=no -r ./* danil@192.168.1.103:$DEPLOY_DIR
 
 ssh -i "$HOME/.ssh/key" -o StrictHostKeyChecking=no "$USER@$IP_ADDRESS" << EOF
+docker run -d \
+  --name watchtower \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  containrrr/watchtower \
+  --interval 300
 cd $DEPLOY_DIR
-cat > .env << EOL
-TELEGRAM_BOT_TOKEN=${TELEGRAM_BOT_TOKEN}
-CHANNEL_ID=${CHANNEL_ID}
-ADMIN_CHAT_ID=${ADMIN_CHAT_ID}
-SECRET_KEY=${SECRET_KEY}
-BASE_URL=${BASE_URL}
-EOL
 docker compose build
 docker compose up -d
 EOF
