07f2103f590b83ee6d161b3dd1f6b002f37b52b4
danil <danildeis100@mail.ru>
2025-05-06 00:12:34 +0700

    server13

--- project/deploy.sh
+++ project/deploy.sh
@@ -7,12 +7,16 @@ DEPLOY_DIR="/home/danil/project"
 scp -i "$HOME/.ssh/key" -o StrictHostKeyChecking=no -r ./* "$USER@$IP_ADDRESS":$DEPLOY_DIR
 
 ssh -i "$HOME/.ssh/key" -o StrictHostKeyChecking=no "$USER@$IP_ADDRESS" << EOF
-docker run -d \
-  --name watchtower \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  containrrr/watchtower \
-  --interval 300
-cd $DEPLOY_DIR
-docker compose build
-docker compose up -d
+ cd $DEPLOY_DIR
+  docker compose build
+  docker compose up -d
+
+  # Запускаем watchtower, если он не запущен
+  if ! docker ps --filter "name=watchtower" --format '{{.Names}}' | grep -q watchtower; then
+    docker run -d \
+      --name watchtower \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      containrrr/watchtower \
+      --interval 300
+  fi
 EOF
