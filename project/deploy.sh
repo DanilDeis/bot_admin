f54eaad71187c1f4a931f2fed17995c6380741fa
danil <danildeis100@mail.ru>
2025-05-05 21:22:19 +0700

    373

--- project/deploy.sh
+++ project/deploy.sh
@@ -9,11 +9,11 @@ scp -i "$HOME/.ssh/key" -o StrictHostKeyChecking=no -r ./* danil@192.168.1.103:$
 ssh -i "$HOME/.ssh/key" -o StrictHostKeyChecking=no "$USER@$IP_ADDRESS" << EOF
 cd $DEPLOY_DIR
 cat > .env << EOL
-TELEGRAM_BOT_TOKEN=7460006864:AAFSkhVWk0IwgZjK8fuBZx5Jk4F7X_oWkjM
-CHANNEL_ID=-1002228367571
-ADMIN_CHAT_ID=877631642
-SECRET_KEY=11b67d7be7d567a94b2b1bcc612f71e06de31d55d85af8154e552713ee96ae36
-BASE_URL=https://payform.ru/u77coTH/
+TELEGRAM_BOT_TOKEN=${TELEGRAM_BOT_TOKEN}
+CHANNEL_ID=${CHANNEL_ID}
+ADMIN_CHAT_ID=${ADMIN_CHAT_ID}
+SECRET_KEY=${SECRET_KEY}
+BASE_URL=${BASE_URL}
 EOL
 docker compose build
 docker compose up -d
