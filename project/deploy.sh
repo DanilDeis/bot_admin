61f42c6280346485780493c98ba593f802ec642a
danil <danildeis100@mail.ru>
2025-05-06 15:43:53 +0700

    server_start46

--- project/deploy.sh
+++ project/deploy.sh
@@ -12,7 +12,6 @@ scp -i "$HOME/.ssh/id_rsa" -o StrictHostKeyChecking=no -r . "$USER@$IP_ADDRESS":
 
 ssh -i "$HOME/.ssh/id_rsa" -o StrictHostKeyChecking=no "$USER@$IP_ADDRESS" << EOF
 
-
   cd $DEPLOY_DIR
   echo "Содержимое директории после копирования:"
   ls -la
