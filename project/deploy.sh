770dd3eb7fecf58fd7905c34df1f6234ba80a237
danil <danildeis100@mail.ru>
2025-05-06 14:48:16 +0700

    server_start24

--- project/deploy.sh
+++ project/deploy.sh
@@ -3,18 +3,19 @@
 USER="root"
 IP_ADDRESS="217.25.90.119"
 DEPLOY_DIR="/home/danil/project"
-START_DIR="/home/danil"
+
 # Создаём директорию и файл users.db на сервере
 ssh -i "$HOME/.ssh/id_rsa" -o StrictHostKeyChecking=no "$USER@$IP_ADDRESS" "mkdir -p $DEPLOY_DIR && touch $DEPLOY_DIR/users.db"
 
-# Копируем все необходимые файлы в директорию проекта на сервере
-scp -i "$HOME/.ssh/id_rsa" -o StrictHostKeyChecking=no docker-compose.yml .env users.db "$USER@$IP_ADDRESS":$DEPLOY_DIR
+# Копируем весь проект в директорию проекта на сервере
 scp -i "$HOME/.ssh/id_rsa" -o StrictHostKeyChecking=no -r ./* "$USER@$IP_ADDRESS":$DEPLOY_DIR
 
 # Выполняем команды на сервере
 ssh -i "$HOME/.ssh/id_rsa" -o StrictHostKeyChecking=no "$USER@$IP_ADDRESS" << EOF
-
   cd $DEPLOY_DIR
+
+  echo "Содержимое директории после копирования:"
+  ls -la
 
   docker-compose build
   docker-compose up -d
