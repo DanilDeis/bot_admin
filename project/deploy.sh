2eced4ce8596299fb615cd5983c8c56d5cbf6c12
danil <danildeis100@mail.ru>
2025-05-06 14:06:24 +0700

    server_start11

--- project/deploy.sh
+++ project/deploy.sh
@@ -10,13 +10,13 @@ scp -i "$HOME/.ssh/id_rsa" -o StrictHostKeyChecking=no ../docker-compose.yml ../
 scp -i "$HOME/.ssh/id_rsa" -o StrictHostKeyChecking=no -r ./* "$USER@$IP_ADDRESS":$DEPLOY_DIR
 ssh -i "$HOME/.ssh/id_rsa" -o StrictHostKeyChecking=no "$USER@$IP_ADDRESS" << EOF
 
- cd $START_DIR
+  cd $DEPLOY_DIR
 
-  # Используйте docker-compose или docker compose в зависимости от версии
-  docker-compose build
-  docker-compose up -d
+  # Строим и запускаем контейнеры
+  docker compose build
+  docker compose up -d
 
-  # Запускаем watchtower, если не запущен
+  # Запускаем watchtower, если он не запущен
   if ! docker ps --filter "name=watchtower" --format '{{.Names}}' | grep -q watchtower; then
     docker run -d \
       --name watchtower \
