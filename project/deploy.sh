879761336224e392e98cb03ffd7f5a8caa737e60
danil <danildeis100@mail.ru>
2025-05-06 15:06:36 +0700

    server_start33

--- project/deploy.sh
+++ project/deploy.sh
@@ -6,7 +6,6 @@ DEPLOY_DIR="/home/danil/project"
 
 echo "Локальные файлы для копирования:"
 ls -la
-
 ssh -i "$HOME/.ssh/id_rsa" -o StrictHostKeyChecking=no "$USER@$IP_ADDRESS" "mkdir -p $DEPLOY_DIR && touch $DEPLOY_DIR/users.db"
 
 scp -i "$HOME/.ssh/id_rsa" -o StrictHostKeyChecking=no -r ./* "$USER@$IP_ADDRESS":$DEPLOY_DIR
